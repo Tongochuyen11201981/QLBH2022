e65e8fd75fd4fc5f9f6e5e743f71946eefd39076
Tô Ngọc Huyền <ngochuyen.to2002@gmail.com>
2022-08-22 13:00:03 +0700

    Tôi hiểu cách làm r bé ơi

--- QLBH2022/Frmabout.Designer.cs
+++ QLBH2022/Frmabout.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -52,12 +53,23 @@
             this.label2.Size = new System.Drawing.Size(0, 25);
             this.label2.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.BlueViolet;
+            this.button1.Location = new System.Drawing.Point(138, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(459, 79);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Toi lam duoc roi ban oi";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Frmabout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.Black;
@@ -73,6 +85,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
